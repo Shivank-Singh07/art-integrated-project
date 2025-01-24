1f44f1b2c845a2f513029964eaa1fccce55c4869
Shivank Singh Thakur <shivanksinghthakur2012@gmail.com>
2025-01-24 22:37:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+document.getElementById("damphu").addEventListener("click", () => {
+    var audio = new Audio('assets/audios/damphu.mp3');
+    audio.play();
+})
+
+document.getElementById("khartaal").addEventListener("click", () => {
+    var audio = new Audio('assets/audios/khartaal.mp3');
+    audio.play();
+})
+
+document.getElementById("bansuri").addEventListener("click", () => {
+    var audio = new Audio('assets/audios/bansuri.mp3');
+    audio.play();
+})
+
+document.getElementById("dhol").addEventListener("click", () => {
+    var audio = new Audio('assets/audios/dhol.mp3');
+    audio.play();
+})
+
+document.getElementById("tala").addEventListener("click", () => {
+    var audio = new Audio('assets/audios/tala.mp3');
+    audio.play();
+})
+
+document.getElementById("sarangi").addEventListener("click", () => {
+    var audio = new Audio('assets/audios/sarangi.mp3');
+    audio.play();
+})
